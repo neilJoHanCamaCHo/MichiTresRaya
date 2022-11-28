4bfbcf2065647b5e1a61fb0ac9b04c51898014a1
neilJoHanCamaCHo <neilcamacho@gmail.com>
2022-11-28 02:22:00 -0500

    TresRay

---
+++ src/main/java/pe/edu/upeu/MichiTresRaya.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pe.edu.upeu;
+
+import pe.edu.upeu.gui.MainTresRaya;
+/**
+ *
+ * @author neilc
+ */
+public class MichiTresRaya {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new MainTresRaya().setVisible(true);
+    }
+}
